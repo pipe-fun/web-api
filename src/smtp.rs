d3863a873276118b752e5bb991b568a523a187af
Spxg <itsme@unsafe.me>
2020-09-13 11:59:06 +0800

    update email config from .env

--- src/smtp.rs
+++ src/smtp.rs
@@ -4,6 +4,13 @@ use lettre::smtp::error::SmtpResult;
 use native_tls::TlsConnector;
 use lettre_email::{EmailBuilder, Email};
 use lettre_email::error::Error;
+use dotenv_codegen::dotenv;
+use std::str::FromStr;
+
+const SMTP_ADDRESS: &str = dotenv!("SMTP_ADDRESS");
+const SMTP_PORT: &str = dotenv!("SMTP_PORT");
+const EMAIL_USER: &str = dotenv!("EMAIL_USER");
+const EMAIL_PASSWORD: &str = dotenv!("EMAIL_PASSWORD");
 
 pub enum EmailType {
     Active,
@@ -18,10 +25,6 @@ pub fn check_email(to: &str) -> Result<Email, Error> {
 }
 
 pub fn send_email(to: &str, email_type: EmailType, message: &str) -> SmtpResult {
-    let smtp_address = "xxx";
-    let username = "xxxx";
-    let password = "xxxx";
-
     let subject = match email_type {
         EmailType::Active => "active your account",
         EmailType::ChangePassword => "check code",
@@ -29,18 +32,19 @@ pub fn send_email(to: &str, email_type: EmailType, message: &str) -> SmtpResult 
 
     let email = EmailBuilder::new()
         .to(to)
-        .from(username)
+        .from(EMAIL_USER)
         .subject(subject)
         .text(message)
         .build()
         .unwrap()
         .into();
 
-    let credentials = (username, password).into_credentials();
-    let tls = ClientTlsParameters::new(smtp_address.into(), TlsConnector::new().unwrap());
+    let credentials = (EMAIL_USER, EMAIL_PASSWORD).into_credentials();
+    let tls = ClientTlsParameters::new(SMTP_ADDRESS.into(), TlsConnector::new().unwrap());
     let client_secure = ClientSecurity::Required(tls);
 
-    let mut client = SmtpClient::new((smtp_address, 587), client_secure)
+    let mut client = SmtpClient::new((SMTP_ADDRESS, u16::from_str(SMTP_PORT)
+                                          .unwrap_or_else(|_| 587)), client_secure)
         .unwrap()
         .credentials(credentials)
         .smtp_utf8(true)
